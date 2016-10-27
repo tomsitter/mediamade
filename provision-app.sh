083ae2eeb4d74245868a8501339febbcec8fffdc
Tom Sitter <thomas.sitter@gmail.com>
2016-10-26 23:42:55 -0400

    Add nginx and SSL stuff to vagrantfile (commented out)

--- provision-app.sh
+++ provision-app.sh
@@ -8,11 +8,28 @@ sudo apt-get update
 # Download node source
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-# Install node and MongoDB
-sudo apt-get install -y nodejs mongodb-org
+sudo apt-get install -y nodejs mongodb-org nginx
+
+# Create secure SSL key (takes a minute or 2)
+# sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+# sudo apt-get install git bc
+# sudo git clone https://github.com/letsencrypt/letsencrypt
+# cd /opt/letsencrypt
+# ./letsencrypt-auto certonly --standalone -d mediamade.me -d www.mediamade.me
+
+# Need to copy files from host to right files in question
+# cd /var/www/mediamade
+
+# sudo cp ./config/nginx/sites-enabled/default /etc/nginx/sites-enabled/default
+# sudo cp ./config/nginx/snippets/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
+
+# sudo service nginx start
 
 # Install npm packages
-cd /var/www/mediamade
 npm install
+
+sudo npm install -g pm2
+pm2 start www/bin
 
 echo "cd /var/www/mediamade" >> /home/vagrant/.bashrc
